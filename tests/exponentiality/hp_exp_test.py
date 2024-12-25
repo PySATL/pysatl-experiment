21732863ff47f4383e4dffcd3262aa78f6615806
Dmitri <dmitrvlkuznetsov@gmail.com>
2024-12-25 06:22:23 +0300

    test: added tests for exponentiality

--- tests/exponentiality/hp_exp_test.py
+++ tests/exponentiality/hp_exp_test.py
@@ -4,14 +4,16 @@ from stattest.test.exponent import HPTestExp
 from tests.exponentiality.abstract_exponentiality_test_case import AbstractExponentialityTestCase
 
 
-@pytest.mark.parametrize(  # TODO: actual test (7; 10)
+@pytest.mark.parametrize(
     ("data", "result"),
     [
-        ([1, 2, 3, 4, 5, 6, 7], 0.17377394345044517),
-        ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0.16232061118184815),
+        ([1, 2, 3, 4, 5, 6, 7], 0.12380952380952381),
+        ([i for i in range(1, 10)], 0.1349206349206349),
+        ([i for i in range(1, 50)], 0.14644423212420637),
+        ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0),  # Zero exception test
+        ([-4, -1, -6, -8, -4, -2, 0, -2, 0, -3], 0.16232061118184815),  # Negative values test
     ],
 )
-@pytest.mark.skip(reason="fix test and check")
 class TestCaseHPExponentialityTest(AbstractExponentialityTestCase):
     @pytest.fixture
     def statistic_test(self):
