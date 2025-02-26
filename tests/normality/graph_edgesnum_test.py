6baca7c96bc950b6fd4869fd39d6a45d75fc9ad8
Ivan Pokhabov <vanek3372@gmail.com>
2025-02-26 13:30:31 +0300

    fix: fix tests after changing criterion

--- tests/normality/graph_edgesnum_test.py
+++ tests/normality/graph_edgesnum_test.py
@@ -7,8 +7,8 @@ from tests.normality.abstract_normality_test_case import AbstractNormalityTestCa
 @pytest.mark.parametrize(
     ("data", "result"),
     [
-        ([-0.264, 0.031, 0.919, 1.751, -0.038, 0.133, 0.643, -0.480, 0.094, -0.527], 9),
-        ([1.311, 3.761, 0.415, 0.764, 0.100, -0.028, -1.516, -0.108, 2.248, 0.229], 11),
+        ([-0.264, 0.031, 0.919, 1.751, -0.038, 0.133, 0.643, -0.480, 0.094, -0.527], 18),
+        ([1.311, 3.761, 0.415, 0.764, 0.100, -0.028, -1.516, -0.108, 2.248, 0.229], 6),
     ],
 )
 class TestCaseGraphEdgesNumbernormalityTest(AbstractNormalityTestCase):
