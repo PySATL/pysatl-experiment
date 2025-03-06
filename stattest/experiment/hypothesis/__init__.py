6899ca3560ff70c349528ea5d45aee83b8dbcdc0
Dmitri <dmitrvlkuznetsov@gmail.com>
2025-03-06 17:10:34 +0300

    test: added unit tests for json config parser

--- stattest/experiment/hypothesis/__init__.py
+++ stattest/experiment/hypothesis/__init__.py
@@ -1,4 +1,4 @@
-from stattest.experiment.hypothesis.hypothesis import (NormalHypothesis, WeibullHypothesis)
+from stattest.experiment.hypothesis.hypothesis import NormalHypothesis, WeibullHypothesis
 from stattest.experiment.hypothesis.model import AbstractHypothesis
 
 
--- tests/resolvers/hypothesis_resolver_test.py
+++ tests/resolvers/hypothesis_resolver_test.py
@@ -1,6 +1,18 @@
+import pytest
+
+from stattest.experiment.hypothesis import NormalHypothesis, WeibullHypothesis
 from stattest.resolvers.hypothesis_resolver import HypothesisResolver
 
 
-def test_cauchy_rvs_generator():
-    hypothesis = HypothesisResolver.load_hypothesis("NormalHypothesis")
+@pytest.mark.parametrize(
+    ("name", "expected"),
+    [
+        ("NormalHypothesis", NormalHypothesis),
+        ("WeibullHypothesis", WeibullHypothesis),
+    ],
+)
+def test_load_without_params(name, expected):
+    hypothesis = HypothesisResolver.load(name)
+
     assert hypothesis is not None
+    assert type(hypothesis) is expected
