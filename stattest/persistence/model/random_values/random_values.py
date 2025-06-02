ca6afa9af46470e9765e69974b592c374fa02ccc
Lev Golofastov <l.golofastov@gmail.com>
2025-06-02 03:18:02 +0300

    Add pysatl-criterion as dependency and add database models

---
+++ stattest/persistence/model/random_values/random_values.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import Protocol
+
+from pysatl_criterion.persistence.model.common.data_storage.data_storage import (
+    DataModel,
+    DataQuery,
+    IDataStorage,
+)
+
+
+@dataclass
+class RandomValuesModel(DataModel):
+    experiment_id: int
+    generator_code: str
+    generator_parameters: list[float]
+    sample_size: int
+    sample_num: int
+    data: list[float]
+
+
+@dataclass
+class RandomValuesQuery(DataQuery):
+    generator_code: str
+    generator_parameters: list[float]
+    sample_size: int
+
+
+class IRandomValuesStorage(
+    IDataStorage[RandomValuesModel, RandomValuesQuery], Protocol
+):
+    """
+    Random values storage interface.
+    """
+
+    pass
