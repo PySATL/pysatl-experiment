4eec4d64971035764adf557c49c5a86a7477afdf
Alexey Mironov <alexmir2015@yandex.ru>
2025-12-09 18:10:02 +0300

    Change path to file in configuration connection to connection string

--- pysatl_experiment/cli/commands/configure/storage_connection/storage_connection.py
+++ pysatl_experiment/cli/commands/configure/storage_connection/storage_connection.py
@@ -1,7 +1,6 @@
 from click import Context, argument, echo, pass_context
 
 from pysatl_experiment.cli.commands.common.common import (
-    create_storage_path,
     get_experiment_name_and_config,
     save_experiment_config,
 )
@@ -16,14 +15,14 @@ def storage_connection(ctx: Context, connection: str) -> None:
     Configure storage connection.
 
     :param ctx: context.
-    :param connection: storage connection.
+    :param connection: storage connection. Example: postgresql://postgres:postgres@localhost/pysatl
     """
 
     experiment_name, experiment_config = get_experiment_name_and_config(ctx)
 
-    storage_path = create_storage_path(connection)
-    experiment_config["storage_connection"] = storage_path
+    #storage_path = create_storage_path(connection)
+    experiment_config["storage_connection"] = connection
 
     save_experiment_config(ctx, experiment_name, experiment_config)
 
-    echo(f"Storage connection of the experiment {experiment_name} is set to '{storage_path}'.")
+    echo(f"Storage connection of the experiment {experiment_name} is set to '{connection}'.")
