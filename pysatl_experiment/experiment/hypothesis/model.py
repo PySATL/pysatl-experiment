9aaa306fba4eebf9872642f7e57aa0377515794e
Dmitri Kuznetsov <dmitrvlkuznetsov@gmail.com>
2026-04-20 02:07:05 +0300

    chore: deleted unused stuff

--- pysatl_experiment/experiment/hypothesis/model.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class AbstractHypothesis(ABC):
-    @abstractmethod
-    def generate(self, size, **kwargs):
-        raise NotImplementedError("Method is not implemented")
